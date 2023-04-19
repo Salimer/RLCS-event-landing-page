a9b2eea8552b449973529cc75d077430ae5ccd5f
Salimer <52242629+Salimer@users.noreply.github.com>
2023-04-19 16:00:53 +0300

    test

--- .history/script_20230419160043.js
+++ .history/script_20230419160043.js
@@ -1,7 +1,7 @@
 const featuredTeams = [
   {
     image: 'Assets/teams/spacestation.png',
-    name: 'NRG Esports .png',
+    name: 'NRG Esports ',
     region: '(North America)',
     description: 'NRG Esports is a top-performing North American team that has won several major Rocket League tournaments. The team consists of players GarrettG, jstn, and SquishyMuffinz.',
   },
